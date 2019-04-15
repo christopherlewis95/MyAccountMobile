61199a192ef6e014617bccfa2305d2b5db163095
christopherlewis95 <christopherlewis@nevada.unr.edu>
2019-04-15 07:10:17 -0700

    Initial commit

---
+++ www/scripts/index.js
@@ -0,0 +1,54 @@
+﻿var app = {
+    // Application Constructor
+    initialize: function () {
+        this.bindEvents();
+    },
+    bindEvents: function () {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    onDeviceReady: function () {
+        app.receivedEvent('deviceready');
+
+        // Here, we redirect to the web site.
+        var targetUrl = "https://myaccount.lvvwd.com";
+        var bkpLink = document.getElementById("bkpLink");
+        bkpLink.setAttribute("href", targetUrl);
+        bkpLink.text = targetUrl;
+        window.location.replace(targetUrl);
+    },
+    // Note: This code is taken from the Cordova CLI template.
+    receivedEvent: function (id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+/*
+cordova.plugins.locationAccuracy.request(
+    function () {
+        console.log("testhigh success");
+        setTimeout(function () {
+            getPosition();
+        }, 1500);
+    },
+    function () {
+        console.log("error");
+    },
+    cordova.plugins.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY
+);
+
+function getPosition() {
+    navigator.geolocation.getCurrentPosition(
+        function (position) {
+            var latitude = position.coords.latitude;
+            var longitude = position.coords.longitude;
+        }
+    );
+}
+*/
+app.initialize();
